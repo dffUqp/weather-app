e34d209bc05a3f37b71dc87493f3476a4552a577
dffUqp <yuriivasilchuk.work@gmail.com>
2023-01-02 22:30:14 +0200

    feat(*): initial commit

---
+++ .eslintrc.cjs
@@ -0,0 +1,34 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'airbnb',
+    'airbnb/hooks',
+    'airbnb-typescript',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+  ],
+  overrides: [],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: './tsconfig.json',
+  },
+  plugins: ['react', '@typescript-eslint', 'prettier'],
+  rules: {
+    'react/react-in-jsx-scope': 0,
+    'react/button-has-type': 0,
+    'react/jsx-one-expression-per-line': 0,
+    '@typescript-eslint/no-shadow': 0,
+    'prettier/prettier': [
+      'warn',
+      {
+        endOfLine: 'auto',
+      },
+    ],
+  },
+};
---
+++ src/App.tsx
@@ -0,0 +1,7 @@
+import './App.css';
+
+function App() {
+  return <div className="App text-sky-100">Hello World</div>;
+}
+
+export default App;

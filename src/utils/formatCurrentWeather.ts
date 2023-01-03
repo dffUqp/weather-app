0e86fc8c510f915872ee7d43e6b6e54f64d631cf
dffUqp <yuriivasilchuk.work@gmail.com>
2023-01-03 20:54:19 +0200

    feat(utils): create formatCurrentWeather util

---
+++ src/utils/formatCurrentWeather.ts
@@ -0,0 +1,36 @@
+import { IFormateWeather, IWeatherResponds } from '../ts/weatherTypes';
+
+const formatCurrentWeather = (data: IWeatherResponds): IFormateWeather => {
+  const {
+    coord: { lat, lon },
+    // eslint-disable-next-line @typescript-eslint/naming-convention
+    main: { temp, feels_like, temp_min, temp_max, humidity },
+    name,
+    dt,
+    weather,
+    sys: { country, sunrise, sunset },
+    wind: { speed },
+  } = data;
+
+  const { main: details, icon } = weather[0];
+
+  return {
+    lat,
+    lon,
+    temp,
+    feels_like,
+    temp_min,
+    temp_max,
+    humidity,
+    name,
+    dt,
+    country,
+    sunrise,
+    sunset,
+    details,
+    icon,
+    speed,
+  };
+};
+
+export default formatCurrentWeather;

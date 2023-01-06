0339ce58968c5e8eb10ac3c18ac3df75779e1d66
dffUqp <yuriivasilchuk.work@gmail.com>
2023-01-06 11:14:48 +0200

    feat(test): add base test for HomePage and create renderWithRedux util

---
+++ src/__test__/HomePage.test.tsx
@@ -0,0 +1,55 @@
+import { QueryClientProvider } from 'react-query';
+import { BrowserRouter } from 'react-router-dom';
+import { describe, it } from 'vitest';
+
+import Home from '../pages/Home';
+import { queryClient } from '../plugins';
+import renderWithRedux from './renderWithRedux';
+
+describe('HomePage', () => {
+  it('should render list', () => {
+    const { getAllByTestId } = renderWithRedux(
+      <QueryClientProvider client={queryClient}>
+        <BrowserRouter>
+          <Home />
+        </BrowserRouter>
+      </QueryClientProvider>,
+      {
+        persistedReducer: {
+          weatherCardsReducer: {
+            weatherCards: [
+              { lat: 37.1289771, lon: -84.0832646 },
+              { lat: 35.6828387, lon: 139.7594549 },
+              { lat: 46.4843023, lon: 30.7322878 },
+            ],
+          },
+        },
+      }
+    );
+
+    const tickers = getAllByTestId('card-item');
+
+    expect(tickers.length).toBe(3);
+  });
+
+  it('should not render list', () => {
+    const { queryAllByTestId } = renderWithRedux(
+      <QueryClientProvider client={queryClient}>
+        <BrowserRouter>
+          <Home />
+        </BrowserRouter>
+      </QueryClientProvider>,
+      {
+        persistedReducer: {
+          weatherCardsReducer: {
+            weatherCards: [],
+          },
+        },
+      }
+    );
+
+    const tickers = queryAllByTestId('card-item');
+
+    expect(tickers.length).toBe(0);
+  });
+});

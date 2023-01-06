964dc5fbb8463bcdbe31d280b50336cc2ad3b391
dffUqp <yuriivasilchuk.work@gmail.com>
2023-01-06 10:53:57 +0200

    feat(deps): implement react-query

--- src/App.tsx
+++ src/App.tsx
@@ -1,54 +1,14 @@
-import { Box, Container, Grid, Paper, TextField } from '@mui/material';
-import Button from '@mui/material/Button';
+import { QueryClientProvider } from 'react-query';
+import MuiWrapper from './components/MuiWrapper';
+import { queryClient } from './plugins';
 
 function App() {
   return (
-    <Container maxWidth="lg">
-      <TextField
-        fullWidth
-        hiddenLabel
-        variant="outlined"
-        size="small"
-        sx={{
-          mt: '5px',
-          // marginX: 'auto',
-          // width: '1000px',
-          '& .MuiInputBase-root': {
-            // backgroundColor: '',
-            color: 'white',
-          },
-        }}
-        id="filled-basic"
-        placeholder="Writte something"
-      />
-      <Box
-        sx={{
-          maxWidth: 1145,
-          width: '100%',
-          minHeight: 650,
-          height: 'auto',
-          marginY: '25px',
-          marginX: 'auto',
-          p: '12px',
-          border: '1px solid black',
-        }}
-      >
-        <Grid container rowSpacing={2} columnSpacing={{ xs: 3, md: 6 }}>
-          {Array.from(Array(6)).map((_, index) => (
-            <Grid item xs={6} sm={4} md={3} key={index}>
-              <Paper
-                sx={{
-                  backgroundColor: 'transparent',
-                  height: '300px',
-                }}
-              >
-                <Button variant="contained">Click me</Button>
-              </Paper>
-            </Grid>
-          ))}
-        </Grid>
-      </Box>
-    </Container>
+    <QueryClientProvider client={queryClient}>
+      <MuiWrapper>
+        <div>Hello World</div>
+      </MuiWrapper>
+    </QueryClientProvider>
   );
 }
 
--- src/plugins/index.ts
+++ src/plugins/index.ts
@@ -1,2 +1,3 @@
 export { default as axios } from './axios';
+export { default as queryClient } from './reactQuery';
 export { default as theme } from './theme';

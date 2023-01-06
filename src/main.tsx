9b50b36ab8273434ee3490af6734289a2d0c5405
dffUqp <yuriivasilchuk.work@gmail.com>
2023-01-06 11:17:44 +0200

    fix(main): delete MuiWrapper

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+
 import App from './App';
 import './assets/styles/main.css';
-import MuiWrapper from './components/MuiWrapper';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <MuiWrapper>
-      <App />
-    </MuiWrapper>
+    <App />
   </React.StrictMode>
 );
